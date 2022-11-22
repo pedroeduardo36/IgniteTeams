771488d2f5ea28c299930b0949747ed80b326aec
Pedro <pedrovilarinho.gpd@gmail.com>
2022-11-22 14:04:57 -0300

    feat styled components

--- App.tsx
+++ App.tsx
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Groups } from "@screens/Groups";
+import { ThemeProvider } from "styled-components/native";
+import theme from "@theme/index";
+import { ActivityIndicator, StatusBar } from "react-native";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={theme}>
+      {fontsLoaded ? <Groups /> : <ActivityIndicator />}
+      <StatusBar barStyle="light-content" />
+    </ThemeProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,5 @@
+import { Container } from "./styles";
+
+export function Groups() {
+  return <Container></Container>;
+}
