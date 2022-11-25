0f1a783697a7cbb330468b59e37b8dba30132708
Pedro <pedrovilarinho.gpd@gmail.com>
2022-11-25 11:48:38 -0300

    feat GroupCard component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,5 +1,6 @@
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
+import { GroupCard } from "@components/GroupCard";
 import { Container } from "./styles";
 
 export function Groups() {
@@ -7,6 +8,7 @@ export function Groups() {
     <Container>
       <Header />
       <Highlight title="Turma" subtitle="Jogue com sua turma"/>
+      <GroupCard title='Grupo do audi'/>
     </Container>
   );
 }
