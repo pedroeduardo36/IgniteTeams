82e4b37ce9cf10ffdc5e967d6da70efe0622949b
Pedro <pedrovilarinho.gpd@gmail.com>
2022-11-29 17:50:33 -0300

    feat Filter Component

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,4 +1,4 @@
-import { Header, Highlight, ButtonIcon, Input } from "@components/index";
+import { Header, Highlight, ButtonIcon, Input, Filter } from "@components/index";
 import { Container, Form } from "./styles";
 
 export function Players() {
@@ -9,10 +9,12 @@ export function Players() {
         title="Nome da turma"
         subtitle="Adicione a galera e separe os times"
       />
+      <Filter title="time 1" isActive/>
       <Form>
         <Input placeholder="Nome do participante" autoCorrect={false} />
         <ButtonIcon icon="check" />
       </Form>
+      
     </Container>
   );
 }
