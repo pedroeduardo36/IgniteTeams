4175ff6c26e9b96a0925a6aa7b8a9a64399c4235
Pedro <pedrovilarinho.gpd@gmail.com>
2022-11-29 13:48:18 -0300

    refact CSS healper

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,4 +1,4 @@
-import styled from "styled-components/native";
+import styled, { css } from "styled-components/native";
 import { TextInput } from "react-native";
 
 export const Container = styled(TextInput)`
@@ -9,8 +9,10 @@ export const Container = styled(TextInput)`
   border-radius: 6px;
   padding: 16px;
 
-  background-color: ${({ theme }) => theme.COLORS.GRAY_700};
-  color: ${({ theme }) => theme.COLORS.WHITE};
-  font-size: ${({ theme }) => theme.FONT_SIZE.MD}px;
-  font-family: ${({ theme }) => theme.FONT_FAMILY.REGULAR};
+  ${({ theme }) => css`
+    background-color: ${theme.COLORS.GRAY_700};
+    color: ${theme.COLORS.WHITE};
+    font-size: ${theme.FONT_SIZE.MD}px;
+    font-family: ${theme.FONT_FAMILY.REGULAR};
+  `};
 `;
