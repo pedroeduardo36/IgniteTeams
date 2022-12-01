5e3d3a6830310dbde7bf6340c27e070c663d80db
Pedro <pedrovilarinho.gpd@gmail.com>
2022-12-01 18:00:59 -0300

    feat groups to newGroup navigation

--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -1,10 +1,7 @@
 import styled from "styled-components/native";
 
-export const Container = styled.SafeAreaView`
-  height: 100%;
-  width: 100%;
-  background-color: ${({theme}) => theme.COLORS.GRAY_600};
+export const Container = styled.View`
+  flex: 1;
+  background-color: ${({ theme }) => theme.COLORS.GRAY_600};
   padding: 24px;
-  align-items: center;
 `;
-
