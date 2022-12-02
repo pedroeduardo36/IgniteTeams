f23e651ede9bc749fe4904af110e5b40734f07df
Pedro <pedrovilarinho.gpd@gmail.com>
2022-12-02 10:13:42 -0300

    fix navigation glitch

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -3,13 +3,17 @@ import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { Input } from "@components/Input";
 import { useNavigation } from "@react-navigation/native";
+import { useState } from "react";
 import { Container, Content, Icon } from "./styles";
 
 export function NewGroup() {
+  const [group, setGroup] = useState("");
+
   const navigation = useNavigation();
 
   function handleNew() {
-    navigation.navigate("players", { group: "Rocket" });
+    navigation.navigate("players", { group });
+    
   }
 
   return (
@@ -21,7 +25,7 @@ export function NewGroup() {
           title="Nova turma"
           subtitle="Crie a turma para adicionar pessoas"
         />
-        <Input placeholder="Nome da turma" />
+        <Input placeholder="Nome da turma" onChangeText={setGroup} />
         <Button
           title="Criar nova turma"
           style={{ marginTop: 20 }}
